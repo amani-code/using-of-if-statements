52a2581bc6a25a061bff1c0ff163d0b06f3e0592
amani-code <85559277+amani-code@users.noreply.github.com>
2021-12-10 20:21:40 +0300

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,18 @@
+//137220 amani shaka chege
+//using if statements
+
+#include <iostream>
+int main()
+{
+	int age;
+	cout<<"Please enter your childs age:\n";
+	cin>> age;
+
+	if (age>=5){
+		cout<< "admit child to school.";
+
+	} else 
+		cout<<"cancel childs addmision";
+
+	return 0;
+}
